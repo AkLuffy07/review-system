faeec9f8340115eba56fb138eb157c387b226b54
Akhil <akhilsringeri123@gmail.com>
2025-02-17 20:03:08 +0530

    A simple review system app

---
+++ script.js
@@ -0,0 +1,86 @@
+const starRating = document.getElementById('rating')
+const textArea = document.getElementById('text-area')
+const reviewForm = document.getElementById('reviewForm')
+
+let userReview = {
+    rating: 0,
+    text: '',
+    date: new Date(),
+}
+
+const startLength = 5;
+
+function starsAvailable() {
+    for (let i = 0; i < startLength; i++) {
+        const star = document.createElement('i')
+        star.classList.add('fa-regular', 'fa-star', 'star_icon')
+        star.id = `star-${i + 1}`
+        starRating.appendChild(star)
+    }
+}
+
+starsAvailable();
+
+const allStars = document.querySelectorAll('.star_icon')
+// console.log(allStars);
+
+let currentRating = 0;
+updateStars = (totalStars) => {
+    userReview.rating = totalStars;
+    
+    allStars.forEach((star, index) => {
+            star.classList.toggle('fa-solid', index < totalStars);
+            star.classList.toggle('fa-regular', index >= totalStars);
+    })    
+}
+
+allStars.forEach((star, index) => {
+    star.addEventListener('click', () => {
+        currentRating = currentRating === index + 1 ? 0 : index + 1;
+        updateStars(currentRating);
+    })
+})
+
+formatDateTime = () => {
+    const date = new Date();
+    const isoString = date.toISOString();
+    const [year, month, day] = isoString.split('T')[0].split('-');
+    const formattedDate = `${day}/${month}/${year}`;
+    const formatter = new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    const formattedTime = formatter.format(date);
+    return `${formattedDate} ${formattedTime}`
+}
+
+reviewForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(currentRating === 0) {
+        return ;
+    }
+    userReview = {
+        rating: currentRating,
+        text: textArea.value,
+        date: formatDateTime()
+    }
+
+
+    const userReviewHTML = `
+        <div class="review_content">
+            <div class="review_date">
+                <img width="15px" src="calender.svg" alt="Calender">
+                <span class="date_time_text">${userReview.date}</span>
+            </div>
+            <div class="review_display">
+                <p class="review_paragraph">${userReview.text}</p>
+                <span class="rating_number">${userReview.rating}</span>
+            </div>
+        </div>
+    `
+
+    const reviewContainer = document.querySelector('.review-container');
+    reviewContainer.innerHTML += userReviewHTML;
+
+    console.log("userReview: ", userReview);
+
+    
+})
+
